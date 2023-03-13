dd162bf56e1b61b69424be323df77d8e072f4703
JLake310 <0310kjy@gmail.com>
2023-03-13 13:21:44 +0900

    [Add] Add main Canvas

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,8 @@ import Canvas from "@components/Canvas";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Canvas></Canvas>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Canvas></Canvas>
     </div>
   );
 }
